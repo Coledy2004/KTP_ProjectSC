b3d865ecc5e4ef338af4148180c3e3a6875584e1
Coledy2004 <coyo5211@colorado.edu>
2025-09-26 13:07:02 -0600

    netflix permissions again

--- FA2025KTP/content.js
+++ FA2025KTP/content.js
@@ -1,9 +1,17 @@
 // content script example: highlights all paragraphs when asked
 console.log('KTP content script loaded on', location.href);
 
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  if (msg && msg.action === 'highlight-paragraphs') {
-    document.querySelectorAll('p').forEach(p => p.style.background = 'yellow');
-    sendResponse({status: 'done'});
+
+//chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+//  if (msg && msg.action === 'highlight-paragraphs') {
+//    document.querySelectorAll('p').forEach(p => p.style.background = 'yellow');
+//    sendResponse({status: 'done'});
+//  }
+//});
+
+function getTitle() {
+  const title = document.querySelector('.video-title h4, .ellipsize-text');
+  if (title) {
+    console.log("Now watching:", title.innerText);
   }
-});
+}
